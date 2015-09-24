7efb1eb8ff2bccadbd72a55f5a550251e51e5940
81k5_Pr0g3r <any@dropmail.me>
2015-09-24 04:52:15 -0400

    Added new function <print()>

--- First.java
+++ First.java
@@ -3,7 +3,11 @@ public class First{
 	public static void main(String[] args){
 
 		System.out.println("Hello world");
+		print();
+	}
 
+	public static void print(){
+		System.out.println("We are in function <print> :-)");
 	}
 
 
